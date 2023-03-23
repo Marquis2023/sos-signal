8c0580bb47d03c385bf0750905e97ef0905cabea
Marquis2023 <126812558+Marquis2023@users.noreply.github.com>
2023-03-23 13:14:43 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Help")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("text m")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("SOS")
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
