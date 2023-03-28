4dd16ba0f58c32010bd0931794ebd16bc653a10c
Marquis2023 <126812558+Marquis2023@users.noreply.github.com>
2023-03-28 13:15:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,32 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    music.playMelody("- - C5 - - - - - ", 120)
+})
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Help")
+    basic.clearScreen()
+    radio.sendString("SOS")
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.clearScreen()
+    radio.sendString("w r u")
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("text m")
+    basic.clearScreen()
+    radio.sendString("hi")
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("SOS")
+    basic.clearScreen()
+    radio.sendString("HELP")
 })
-input.onGesture(Gesture.Shake, function () {
-    basic.showString("Hello!")
+input.onGesture(Gesture.TiltRight, function () {
+    basic.clearScreen()
+    radio.sendString("wsp")
 })
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.clearScreen()
+    radio.sendString("ft")
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    radio.sendString("h r u")
+})
+radio.setGroup(35)
